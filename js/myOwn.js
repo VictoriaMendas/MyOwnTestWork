7f0eb0bae4392ce1480c4bd625d64be0e7c21410
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2023-04-27 20:36:17 +0200

    1

---
+++ js/myOwn.js
@@ -0,0 +1,12 @@
+function splitMessage(message, delimiter) {
+    let words;
+    // Change code below this line
+  splitMessage["Mango hurries to the train"];
+    console.log(words.split(" "));
+    splitMessage["Mango"];
+    console.log(message.split(""));
+   lsplitMessage["best_for_week"];
+    console.log(words.split("_"));
+    // Change code above this line
+    return words;
+  }
