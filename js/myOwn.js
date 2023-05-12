7d74bc22913bdda4b7cd64a4f27ec701868af7ea
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2023-05-12 20:26:13 +0200

    004

--- js/myOwn.js
+++ js/myOwn.js
@@ -116,3 +116,15 @@
 // };
 // console.log(obj.potions);
 // console.log({} === {});
+function getCommonElements(firstArray, secondArray) {
+  const commonElements = [];
+  // Change code below this line
+  firstArray.forEach((element) => {
+    if (secondArray.includes(element)) {
+      commonElements.push(element);
+    }
+  });
+
+  return commonElements;
+  // Change code above this line
+}
---
+++ js/myOwn@2.js
@@ -0,0 +1,83 @@
+// Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+// значення '1', то у змінну result запишемо 'зима', якщо має значення
+// '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+// const num = 3;
+// let result;
+// switch (num) {
+//   case 1:
+//     result = "winter";
+//     break;
+
+//   case 2:
+//     result = "spring";
+//     break;
+
+//   case 3:
+//     result = "summer";
+//     break;
+
+//   case 4:
+//     result = "autumn";
+//     break;
+
+//   default:
+//     result = "error";
+// }
+// console.log(result);
+
+// Напишіть if..else, що відповідає наступному switch:
+
+// switch (browser) {
+//   case 'Edge':
+//     alert( "You've got the Edge!" );
+//     break;
+
+//   case 'Chrome':
+//   case 'Firefox':
+//   case 'Safari':
+//   case 'Opera':
+//     alert( 'Okay we support these browsers too' );
+//     break;
+
+//   default:
+//     alert( 'We hope that this page looks ok!' );
+// // }
+// const browser = "Chrome";
+// if (browser === "Edge") {
+//   alert("You've got the Edge!");
+// } else if (
+//   browser === "Chrome" ||
+//   browser === "Firefox" ||
+//   browser === "Safari" ||
+//   browser === "Opera"
+// ) {
+//   alert("Okay we support these browsers too");
+// } else {
+//   alert("We hope that this page looks ok!");
+// }
+
+// Task #3
+
+//Напишіть цикл, який виводить у консоль
+// числа від max до min за спаданням
+// Додайте усі парні числа від min до max
+
+// function fn(max, min) {
+//   let sum = 0;
+//   for (let i = max; i >= min; i -= 1) {
+//     console.log(i);
+
+//     if (i % 2 === 0) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// }
+// console.log(fn(10, 0));
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] % 2 === 0) {
+//     numbers[i] = numbers[i] + value;
+//   }
+// }
+// Change code above this line
