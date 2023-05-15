5d7be2c215c1bdd7808663cdd998c2498739c5fd
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2023-05-15 16:46:59 +0200

    005

--- js/myOwn@2.js
+++ js/myOwn@2.js
@@ -81,3 +81,33 @@
 //   }
 // }
 // Change code above this line
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//   { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+// ];
+// // Change code below this line
+
+// const titles = books.map((book) => book.title);
+// console.log(titles);
+
+// #4
+// const getUsersWithAge = (users, minAge, maxAge) =>
+//   users.filter((user) => user.age >= minAge && user.age <= maxAge);
+//  #5 Task
+
+const getActiveUsers = (users) => users.filter((user) => user.isActive);
