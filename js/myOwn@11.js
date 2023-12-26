ac016a8ad5771e97c8a6997402fc75fa6d644e9d
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2023-12-26 12:59:42 +0100

    rih

--- js/myOwn@11.js
+++ js/myOwn@11.js
@@ -1,10 +1,153 @@
 // Завдання 1
 // Натискання на кнопку "SHOW ME" має виводити значення
 // з поля введення (дивіться на елементи в html-розмітці)
-const alertButton = document.querySelector("#alert-button");
-const alertInput = document.querySelector("#alert-input")
+// const alertButton = document.querySelector("#alertButton");
+// const alertInput = document.querySelector("#alertInput");
+// const taskTitle = document.querySelector(".taskTitle");
+// console.log(taskTitle);
+// alertButton.addEventListener("click", onAlertBtnClick);
+// function onAlertBtnClick(e) {
+//   taskTitle.textContent = alertInput.value;
+// }
+// Завдання 2
+// Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
+// Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
 
-alertButton.addEventListener("input", onClickInput)
- function onClickInput(e) {
-    console.log(e.target)
- }
+// const swapButton = document.querySelector("#swapButton");
+// const leftSwapInput = document.querySelector("#leftSwapInput");
+// const rightSwapInput = document.querySelector("#rightSwapInput");
+
+// swapButton.addEventListener("click", onSwapButtonClick);
+// function onSwapButtonClick(e) {
+//   console.log(swapButton);
+
+//   const leftInputValue = leftSwapInput.value;
+//   leftSwapInput.value = rightSwapInput.value;
+//   rightSwapInput.value = leftInputValue;
+// }
+
+// Завдання 3
+// Кнопка "Приховати" ховає текст і замінює назву кнопки на
+// "Розкрити", при повторному натисканні текст знову стає доступним
+// і кнопка набуває початкового вигляду.
+
+// const taskTitle = document.querySelector(".taskTitle");
+// const passwordButton = document.querySelector("#passwordButton");
+
+// passwordButton.addEventListener("click", onSwapBtnClick);
+// function onSwapBtnClick(e) {
+//   if (passwordButton.textContent === "Розкрити") {
+//     passwordButton.textContent = "Приховати";
+//     taskTitle.style.display = "block";
+//   } else {
+//     passwordButton.textContent = "Розкрити";
+//     taskTitle.style.display = "none";
+//   }
+// }
+
+// Завдання 4
+// Кнопка "Зменшити" робить квадрат менше на 10 пікселів,
+// kнопка "Збільшити" - більше на 10 пікселів.
+
+// const box = document.querySelector("#box");
+// const decreaseBtn = document.querySelector("#decrease");
+// const increaseBtn = document.querySelector("#increase");
+
+// decreaseBtn.addEventListener("click", onDecreseBtnClick);
+// increaseBtn.addEventListener("click", onIncreaseBtnClick);
+
+// let size = 0;
+// box.style.backgroundColor = "red";
+
+// function onDecreseBtnClick(e) {
+//   if (size === 0) {
+//     return;
+//   }
+//   size -= 10;
+
+//   box.style.width = `${size}px`;
+//   box.style.height = `${size}px`;
+// }
+// function onIncreaseBtnClick(e) {
+//   size += 10;
+//   box.style.width = `${size}px`;
+//   box.style.height = `${size}px`;
+// }
+
+// Завдання 6
+// Натиснувши кнопку "Подвоювати", збільшити значення
+// у кожному елементі списку у 2 рази
+
+// const listItems = document.querySelectorAll(".listItem");
+// const double = document.querySelector("#double");
+// console.log(listItems);
+// double.addEventListener("click", onMultiplyBtnClick);
+
+// function onMultiplyBtnClick(e) {
+
+//   listItems.forEach((item) => {
+//     item.textContent = +item.textContent * 2;
+//   });
+
+// }
+
+// Завдання 8
+// При натисканні на кожну з кнопок підсумовуються значення з data-атрибутів.
+// За натисканням на кнопку "Вивести результат" виводиться сума значення, а також статистика з
+// інформацією про те, яка кнопка була натиснута скільки разів.
+
+// const resultButton = document.querySelector("#resultButton");
+// const resultSection = document.querySelector("#resultSection");
+// const statList = document.querySelector(".statList");
+
+// statList.addEventListener("click", onCalculateBtnClick);
+// resultButton.addEventListener("click", onResultBtnClick);
+
+// const btnStats = {};
+// let sum = 0;
+// function onCalculateBtnClick(e) {
+//   const btnResult = Number(e.target.dataset.number);
+//   sum += btnResult;
+//   btnStats[e.target.textContent] = (btnStats[e.target.textContent] || 0) + 1;
+//   console.log(sum);
+//   console.log(btnStats);
+// }
+// function onResultBtnClick(event) {
+//   resultSection.innerHTML = `${resultButton.textContent} - ${sum}`;
+//   for (const stats in btnStats) {
+//     resultSection.innerHTML += `${stats} була натиснута ${btnStats[stats]} разів `;
+//   }
+// }
+
+// Наведено список людей. Зроби фільтр на ім'я/прізше
+// const users = [
+//   "Peter Miller",
+//   "John Carter",
+//   "Amanda Diaz",
+//   "Brad Sanders",
+//   "Devis Brooks",
+//   "Robert Morgan",
+// ];
+// const contactsFilter = document.querySelector(".contactsFilter");
+// const contactsList = document.querySelector(".contacts");
+
+// renderContacts(users);
+
+// function renderContacts(users) {
+//   const markup = users
+//     .map((user) => `<li class="contact">${user}</li>`)
+//     .join("");
+//   contactsList.innerHTML = markup;
+// }
+
+// contactsFilter.addEventListener("input", onFilteredInputClick);
+
+// function onFilteredInputClick(evt) {
+
+//   const inputValue = evt.target.value.trim().toLowerCase();
+//   const filteredUsers = users.filter((user) =>
+//     user.toLowerCase().includes(inputValue)
+//   );
+//   renderContacts(filteredUsers);
+
+// }
---
+++ js/myOwn@12.js
@@ -0,0 +1,87 @@
+const btnModal = document.querySelector(".header-btn-modal");
+const backdrop = document.querySelector(".backdrop");
+const buttonCloseModal = document.querySelector(".button-close-modal");
+
+btnModal.addEventListener("click", onBtnModalClick);
+buttonCloseModal.addEventListener("click", onButtonCloseModalClick);
+backdrop.addEventListener("click", onBackdropClick);
+
+function onBtnModalClick(e) {
+  openModal();
+}
+
+function onButtonCloseModalClick() {
+  closeModal();
+}
+
+function onBackdropClick(e) {
+  if (e.target !== e.currentTarget) {
+    return;
+  }
+  closeModal();
+}
+
+function onEscPress(e) {
+  if (e.key === "Escape") {
+    closeModal();
+  }
+}
+
+function openModal() {
+  backdrop.classList.remove("is-hidden");
+  document.addEventListener("keydown", onEscPress);
+}
+
+function closeModal() {
+  backdrop.classList.add("is-hidden");
+  document.removeEventListener("keydown", onEscPress);
+}
+
+const cities = [
+  "Бердичів",
+  "Біла Церква",
+  "Бровари",
+  "Житомир",
+  "Запоріжжя",
+  "Івано-Франківськ",
+  "Ізмаїл",
+  "Кам'янське",
+  "Київ",
+  "Коломия",
+  "Кривий Ріг",
+  "Кропивницький",
+  "Луцьк",
+  "Львів",
+  "Миколаїв",
+  "Одеса",
+  "Олександрія",
+  "Полтава",
+  "Рівне",
+  "Суми",
+  "Умань",
+  "Харків",
+  "Черкаси",
+  "Чернігів",
+];
+
+const modalListCity = document.querySelector(".modal-list-city");
+const modalinput = document.querySelector(".modal-input");
+
+renderCity(cities);
+
+function renderCity(cities) {
+  const cityList = cities
+    .map((city) => `<li class="city-input">${city}</li>`)
+    .join("");
+  modalListCity.innerHTML = cityList;
+}
+
+modalinput.addEventListener("input", onInputCitySelectionclick);
+
+function onInputCitySelectionclick(e) {
+  const inputvalue = e.target.value.trim().toLowerCase();
+  const filteredCities = cities.filter((city) =>
+    city.toLowerCase().includes(inputvalue)
+  );
+  renderCity(filteredCities);
+}
