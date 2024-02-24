ea7668989712ef3c054e1915deccae9118c22be8
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2024-02-24 17:15:00 +0100

    hel

--- js/myOwn@14.js
+++ js/myOwn@14.js
@@ -25,30 +25,56 @@ const { email, password } = form.elements;
 form.addEventListener("input", onInputBtnClick);
 form.addEventListener("submit", onSubmitBtnClick);
 
-const STORAGE_KEY = "form-state";
-let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-clickForm();
-console.log(formData);
+const USER_DATA = "form-state";
+textArea();
+// // const localValue = e.target.value;
+// const dataForm = localStorage.getItem(USER_DATA) || {};
+// console.log(dataForm);
+// clickForm();
 
+// function onInputBtnClick(e) {
+//   dataForm[e.target.name] = e.target.value.trim();
+
+//   localStorage.setItem(USER_DATA, JSON.stringify(dataForm));
+// }
+
+// function clickForm() {
+//   const markup1 = ` <button type="button" class="login ">Login</button>`;
+//   const markup2 = `<button type="button" class="logout ">Logout</button>`;
+
+//   if (dataForm) {
+//     try {
+//       const parseItems = JSON.parse(dataForm);
+
+//       email.value = parseItems.email || "";
+//       password.value = parseItems.password || "";
+//       return form.insertAdjacentHTML("afterbegin", markup2);
+//     } catch (error) {
+//       console.error();
+//     }
+//   }
+//   form.insertAdjacentHTML("afterbegin", markup1);
+// }
+// function onSubmitBtnClick(e) {
+//   e.preventDefault();
+//   e.currentTarget.reset();
+//   localStorage.removeItem(USER_DATA);
+// }
 function onInputBtnClick(e) {
-  formData[e.target.name] = e.target.value.trim();
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  const value = e.target.value;
+  localStorage.setItem(USER_DATA, value);
 }
 
-function clickForm() {
-  const localItems = localStorage.getItem(STORAGE_KEY);
-  console.log(localItems);
-  const markup1 = ` <button type="button" class="login ">Login</button>`;
-  const markup2 = `<button type="button" class="logout ">Logout</button>`;
-
-  //   if (!localItems) {
-  //     return form.insertAdjacentHTML("afterbegin", markup2);
-  //   } else {
-  //     form.insertAdjacentHTML("afterbegin", markup1);
-  //   }
-}
 function onSubmitBtnClick(e) {
   e.preventDefault();
   e.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+  localStorage.removeItem(USER_DATA);
+}
+
+function textArea() {
+  const getItem = localStorage.getItem(USER_DATA);
+  if (!getItem) {
+    // form.textarea.value = getItem;
+    alert("Error pls try again");
+  }
 }
