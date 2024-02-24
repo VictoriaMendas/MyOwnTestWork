845bf7d61968f905c60824eecf240888727cb4bb
Vlad <vvllaadd1233345@gmail.co>
2024-02-24 18:05:09 +0100

    made 14th task

--- js/myOwn@14.js
+++ js/myOwn@14.js
@@ -1,54 +1,98 @@
 //  #123
-// Якщо імейл і пароль користувача збігаються, зберігайте дані з
-//  форми при сабмите
-// у локальне сховище і змінюй кнопку login на logout і роби
-//  поля введення Недоступними для зміни.
+// Якщо імейл і пароль користувача збігаються,
+// зберігаєм статус юзера як залогінений в локальне сховище
+// і змінюй кнопку login на logout і роби
+// поля введення Недоступними для зміни.
 
 // При перезавантаженні сторінки, якщо користувач залогінений,
 //  ми маємо бачити logout-кнопку
 // та недоступні для зміни поля з даними користувача.
+
 // Клік по кнопці logout повертає все до початкового вигляду
-// і видаляє дані користувача З локального сховища.
+// і видаляє дані користувача і форми з локального сховища.
 
 // Якщо введені дані не збігаються з потрібними даними,
 // викликати аlert і
 // повідомляти про помилку.
 
-// const USER_DATA = {
-//   email: "user@mail.com",
-//   password: "secret",
-// };
-const form = document.querySelector(".feedback-form");
+const USER_DATA = {
+    email: 'user@mail.com',
+    password: 'secret',
+};
 
-const { email, password } = form.elements;
+const STORAGE_KEY = 'form-state';
+const USER_STATUS = 'user-status';
 
-form.addEventListener("input", onInputBtnClick);
-form.addEventListener("submit", onSubmitBtnClick);
+const form = document.querySelector('.feedback-form');
 
-const STORAGE_KEY = "form-state";
+const { email, password, submitBtn } = form.elements;
+
+form.addEventListener('input', onInputBtnClick);
+form.addEventListener('submit', onSubmitBtnClick);
+
 let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-clickForm();
-console.log(formData);
+let userStatus = JSON.parse(localStorage.getItem(USER_STATUS)) || false;
 
+restoreForm();
+checkUserStatus();
+
+// відновлюємо дані форми коли юзер відкрив сторінку
+function restoreForm() {
+    if (formData) {
+        email.value = formData.email || '';
+        password.value = formData.password || '';
+    }
+}
+
+// перевіряємо статус юзера коли юзер відкрив сторінку
+function checkUserStatus() {
+    if (userStatus) {
+        submitBtn.textContent = 'Logout';
+        email.disabled = true;
+        password.disabled = true;
+    }
+}
+
+// юзер вводить дані
 function onInputBtnClick(e) {
-  formData[e.target.name] = e.target.value.trim();
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    formData[e.target.name] = e.target.value.trim();
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-function clickForm() {
-  const localItems = localStorage.getItem(STORAGE_KEY);
-  console.log(localItems);
-  const markup1 = ` <button type="button" class="login ">Login</button>`;
-  const markup2 = `<button type="button" class="logout ">Logout</button>`;
-
-  //   if (!localItems) {
-  //     return form.insertAdjacentHTML("afterbegin", markup2);
-  //   } else {
-  //     form.insertAdjacentHTML("afterbegin", markup1);
-  //   }
-}
 function onSubmitBtnClick(e) {
-  e.preventDefault();
-  e.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+    e.preventDefault();
+
+    // юзер нажимає Logout
+    if (userStatus) {
+        userStatus = false;
+
+        localStorage.setItem(USER_STATUS, JSON.stringify(userStatus));
+
+        email.disabled = false;
+        password.disabled = false;
+
+        submitBtn.textContent = 'Login';
+
+        return;
+    }
+
+    // юзер нажимає Login
+    if (
+        formData.email === USER_DATA.email &&
+        formData.password === USER_DATA.password
+    ) {
+        userStatus = true;
+
+        localStorage.setItem(USER_STATUS, JSON.stringify(userStatus));
+
+        email.disabled = true;
+        password.disabled = true;
+
+        submitBtn.textContent = 'Logout';
+    } else {
+        alert(
+            `Email "${email.value}" or/and password "${password.value}" is wrong!`
+        );
+    }
 }
